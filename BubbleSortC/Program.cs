d564e75ec76cf00aaeae324aed7bf85b6316431e
Mochamad Aldi Raihan Fachrizal <Onlymarfa69@gmail.com>
2022-10-07 19:57:15 +0700

    Add project files.

---
+++ BubbleSortC/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Bubble_Sort
+{
+    class Program
+    {
+        //Deklarasi array int dengan ukuran 20
+        private int[] a = new int[20];
+
+        // Deklarasi variabel int untuk menyimpan banyaknya data pada array
+        private int n;
+    }
+}
